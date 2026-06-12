b97e0a182e3421c9302219c34f48c24c67170de8
Yegor <egor.grebennykov@gmail.com>
2026-06-12 21:24:33 +0300

    Database Programmability and Transactions

---
+++ Database Programmability and Transactions/money_transfer.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE PROCEDURE sp_withdraw_money(
+    account_id INT,
+    money_amount NUMERIC(10, 4)
+)
+AS
+$$
+DECLARE
+	current_balance NUMERIC;
+BEGIN
+	current_balance := (SELECT balance FROM accounts WHERE id = account_id);
+
+	IF(current_balance >= money_amount) THEN 
+	    UPDATE accounts
+	    SET balance = balance - money_amount
+	    WHERE id = account_id;
+	ELSE
+		RAISE NOTICE 'Insufficient balance to withdraw %', money_amount;
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE sp_deposit_money(
+    account_id INT,
+    money_amount NUMERIC(10, 4)
+)
+AS
+$$
+BEGIN
+    UPDATE accounts
+    SET balance = balance + money_amount
+    WHERE id = account_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE sp_transfer_money(
+    sender_id INT,
+    receiver_id INT,
+    amount NUMERIC(10, 4)
+)
+AS
+$$
+BEGIN
+    CALL sp_withdraw_money(sender_id, amount);
+    CALL sp_deposit_money(receiver_id, amount);
+
+END;
+$$
+LANGUAGE plpgsql;
