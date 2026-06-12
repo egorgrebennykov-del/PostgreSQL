91600c7a6e5e1514579b56727042fbc3f160b79c
Yegor <egor.grebennykov@gmail.com>
2026-06-12 21:59:10 +0300

    Table Relations part 2

---
+++ Table Relations/delete_cascade.sql
@@ -0,0 +1,11 @@
+ALTER TABLE countries
+ADD CONSTRAINT countries_continents_fkey
+FOREIGN KEY (continent_code)
+REFERENCES continents(continent_code)
+ON DELETE CASCADE;
+
+ALTER TABLE countries
+ADD CONSTRAINT countries_currencies_key
+FOREIGN KEY (currency_code)
+REFERENCES currencies(currency_code)
+ON DELETE CASCADE;
